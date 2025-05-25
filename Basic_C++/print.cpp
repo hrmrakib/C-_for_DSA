b71a778192aa0c2378a51d893cf674e12c89a438
hrmrakib <hrmrakibs@gmail.com>
2025-05-25 21:35:37 +0600

    print varibale in c++

---
+++ Basic_C++/print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n = 100;
+
+    char l = 'R';
+
+    string lang = "C++";
+
+    cout << n << " " << l << " " << lang;
+
+    return 0;
+}
