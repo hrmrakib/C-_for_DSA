bf10889b70820de33221ef6125270ba2cb8a8140
hrmrakib <hrmrakibs@gmail.com>
2025-05-25 22:32:24 +0600

    Ternary Operator in c++

---
+++ Basic_C++/EOF.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+// EOF - End Of File (It means taking input end of the all input.)
+
+int main() {
+    
+    int x;
+
+    while (cint >> x)
+    {
+        cout << x << endl;
+    }
+    
+
+    return 0;
+}
---
+++ Basic_C++/Setprecision.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+ 
+int main() {
+    double x = 4.54395;
+
+    cout << fixed << setprecision(2) << x << endl;
+    return 0;
+}
