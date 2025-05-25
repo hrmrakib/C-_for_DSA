2ab9d7183a58512ddd9529dcb962b7475ed33b4d
hrmrakib <hrmrakibs@gmail.com>
2025-05-25 22:13:44 +0600

    typesacting in cpp

--- Basic_C++/take_input_in_C++_Typecasting.cpp
+++ Basic_C++/take_input_in_C++_Typecasting.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 using namespace std;
- 
+
 int main() {
 
     int n;
     char c;
     double d;
 
-    cin >> n >> c >> d;
+    // cin >> n >> c >> d;
 
-    cout << n << " " << c << " "<< d;
+    // cout << n << " " << c << " "<< d;
+
+    // get ASCII
+    char a = 'A';
+    int asciiOfa = a;
+    cout << asciiOfa << endl;
+    // or 
+    cout << (int)a << endl;
 
     return 0;
 }
