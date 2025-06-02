b5f6ad0a2eda94e2588bbab19f6cc8b056992fc9
hrmrakib <hrmrakibs@gmail.com>
2025-06-02 14:46:37 +0600

    ........

--- Week-02_String_Class_Objects/mod-5_String_Class/String_Modifiers.cpp
+++ Week-02_String_Class_Objects/mod-5_String_Class/String_Modifiers.cpp
@@ -4,11 +4,23 @@ using namespace std;
 int main() {
     string s = "Hello ";
     string s2 = "World";
+    string s3 = "Hello World";
 
     // s += s2;
-    s.append(s2);
+    // s.append(s2);
+    // s.push_back('Z');
+    // s.pop_back();
+    // s.pop_back();
+
+    // s = s2;
+    // s.assign("New value assign");
+    // s.erase(2);
+    // s.erase(2,1);
+    s3.replace(6, 5, "Bangladesh");
+    s.insert(6, "Programmer");
 
     cout << s << endl;
+    // cout << s3 << endl;
 
     return 0;
 }
