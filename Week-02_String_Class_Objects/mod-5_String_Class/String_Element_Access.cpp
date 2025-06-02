549b09a565639a168626f899eecf1aa2b74df13a
hrmrakib <hrmrakibs@gmail.com>
2025-06-02 11:40:13 +0600

    string element access

---
+++ Week-02_String_Class_Objects/mod-5_String_Class/String_Element_Access.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Bangladesh";
+
+    cout << s[0] << endl;
+    cout << s.at(0) << endl;
+    cout << s.front() << endl;
+    cout << s.back() << endl;
+    return 0;
+}
---
+++ Week-02_String_Class_Objects/mod-5_String_Class/String_Modifiers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello ";
+    string s2 = "World";
+
+    // s += s2;
+    s.append(s2);
+
+    cout << s << endl;
+
+    return 0;
+}
