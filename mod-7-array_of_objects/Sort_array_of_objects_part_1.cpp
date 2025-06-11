452d86019f8d070bdb4aaee474827610fea8c56e
hrmrakib <hrmrakibs@gmail.com>
2025-06-11 22:37:11 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ mod-7-array_of_objects/Sort_array_of_objects_part_1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+
+    // Sort the array of objects based on marks
+    sort(a, a + n, [](const Student &s1, const Student &s2) {
+        return s1.marks < s2.marks;
+    });
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+
+    return 0;
+}
