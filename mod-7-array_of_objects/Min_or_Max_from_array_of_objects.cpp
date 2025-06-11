f63405d4c99a7612a5cd73a79eb5c13ff66e1255
hrmrakib <hrmrakibs@gmail.com>
2025-06-11 21:56:00 +0600

    Implement multiple code changes for improved functionality and performance

--- mod-7-array_of_objects/Min_or_Max_from_array_of_objects.cpp
+++ mod-7-array_of_objects/Min_or_Max_from_array_of_objects.cpp
@@ -1,27 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Student {
-    public: 
-        string name;
-        int roll;
-        int marks;
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
 };
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
     Student a[n];
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i].name >> a[i].roll >> a[i].marks;
     }
 
-    int min_mark = INT_MAX;
-    for(int i = 0; i < n; i++) {
-        min_mark = min(a[i].marks, min_mark);
+    // Find the minimum marks from the array of objects
+    // int min_mark = INT_MAX;
+    // for(int i = 0; i < n; i++) {
+    //     min_mark = min(a[i].marks, min_mark);
+    // }
+    // cout << min_mark << endl;
+    
+    // Find the student with minimum marks
+    Student mn;
+    mn.marks = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i].marks < mn.marks)
+        {
+            mn = a[i];
+        }
     }
-    cout << min_mark << endl;
+    cout << mn.name << " " << mn.roll << " " << mn.marks << endl;
     return 0;
 }
