7b74cbc1d5d62df6365818559539f4b334b42b07
hrmrakib <hrmrakibs@gmail.com>
2025-06-12 14:45:47 +0600

    ........

---
+++ H_N_Times.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    
+    while(tc--) {
+        // int n;
+        // chat c;
+
+        // for(int i = 0; i < n; i++) {
+
+        // }
+
+        cout << tc << endl;
+    }
+    return 0;
+}
