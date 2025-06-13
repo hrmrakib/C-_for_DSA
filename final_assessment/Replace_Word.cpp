cb75fb2cf2c1dbd74b014282dc90b5f7ecd63b7a
hrmrakib <hrmrakibs@gmail.com>
2025-06-13 22:40:53 +0600

    ...

---
+++ final_assessment/Replace_Word.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    cin.ignore();
+
+    for (int i = 0; i < tc; i++)
+    {
+        string s, x;
+        cin >> s >> x;
+
+        size_t pos = 0;
+        while ((pos = s.find(x, pos)) != string::npos)
+        {
+            s.replace(pos, x.length(), "*");
+            pos += 1;
+        }
+        cout << s << endl;
+    }
+
+    return 0;
+}
